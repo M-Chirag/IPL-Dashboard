09cba62593c7f7aa2a64ad08470c4b7d66f5771a
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2022-06-22 21:13:40 +0530

    Initial commit

---
+++ IPL-workspace/ipl-dashboard/src/main/java/com/example/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,13 @@
+package com.example.ipldashboard.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.ipldashboard.model.Team;
+
+public interface TeamRepository extends CrudRepository<Team, Long>{
+	
+	Team findByTeamName(String teamName);
+
+}
